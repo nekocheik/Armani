42a16f09e25f17a5c0814d8d42c5dc870c31c1b9
Cheik <cheik.kone@hetic.net>
2019-05-01 16:42:37 +0200

    add product page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
-import { articles , produits } from './template';
-
+//import { articles , produits } from './template';
 
 
 const buttonNav= document.querySelector('.nav__barre nav ul');
@@ -9,4 +8,11 @@ buttonNav.addEventListener('click', () => {
   document.querySelector('.nav__barre img').classList.toggle('active')
 } )
 
+const readNext = document.querySelector('.read__next');
+var text = readNext.innerText ;
 
+readNext.addEventListener('click', () => {
+  document.querySelector('.text__description').classList.toggle('active');
+  text = ( ( text === 'Lire la suite' ) ?  'Fermer' : 'Lire la suite' )
+  readNext.innerText = text;
+})
---
+++ src/js/rellax.js
@@ -0,0 +1,11 @@
+
+// ------------------------------------------
+// Rellax.js
+// Buttery smooth parallax library
+// Copyright (c) 2016 Moe Amaya (@moeamaya)
+// MIT license
+//
+// Thanks to Paraxify.js and Jaime Cabllero
+// for parallax concepts
+// ------------------------------------------
+
